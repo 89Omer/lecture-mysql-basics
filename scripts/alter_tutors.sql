c1b6d5fbb2cf20c01a37898df821596139552234
89Omer <mohammad.omer.raza@gmail.com>
2024-08-01 13:56:45 +0100

    Update alter_tutors.sql
    updating records for some tutor names

--- scripts/alter_tutors.sql
+++ scripts/alter_tutors.sql
@@ -1,2 +1,5 @@
 ALTER TABLE `univeristy_db`.`tutors` 
 ADD COLUMN `Designation` VARCHAR(100) NOT NULL DEFAULT 'Lecturer' AFTER `TutorName`;
+
+UPDATE `univeristy_db`.`tutors` SET `Designation` = 'Senior Lecturer' WHERE (`TutorID` = '1');
+UPDATE `univeristy_db`.`tutors` SET `Designation` = 'Program Leader' WHERE (`TutorID` = '4');
